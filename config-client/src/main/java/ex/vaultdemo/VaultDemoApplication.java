61270bb0ecbeab19d2e96dcba92f2cd8c0c55da3
yunjae62 <wodbs7758@naver.com>
2026-08-02 13:21:14 +0900

    config client first commit

---
+++ config-client/src/main/java/ex/vaultdemo/VaultDemoApplication.java
@@ -0,0 +1,12 @@
+package ex.vaultdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultDemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(VaultDemoApplication.class, args);
+    }
+}
