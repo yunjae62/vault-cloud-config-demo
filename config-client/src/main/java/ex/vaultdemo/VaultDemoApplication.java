171d807fd1ae105ade1f8e5678ce1416471dc291
yunjae62 <wodbs7758@naver.com>
2026-08-02 14:35:51 +0900

    feat: config server 로부터 가져온 설정값 출력

--- config-client/src/main/java/ex/vaultdemo/VaultDemoApplication.java
+++ config-client/src/main/java/ex/vaultdemo/VaultDemoApplication.java
@@ -1,10 +1,22 @@
 package ex.vaultdemo;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class VaultDemoApplication {
+
+    @Value("${message}")
+    private String message;
+
+    @PostConstruct
+    void init() {
+        log.info("message from Spring cloud config:{} ", message);
+    }
 
     static void main(String[] args) {
         SpringApplication.run(VaultDemoApplication.class, args);
